1044f950ba6fd2cd5ac1740e28f8e84ba007a7b2
Rita <rita_s@mail.com>
2022-06-23 09:22:49 +0100

    adding templates to other routes

--- server.py
+++ server.py
@@ -26,11 +26,25 @@ def hello():
 # @app.route("/question/<question_id>/edit")
 # def edit_question(question_id):
 #     return render_template('edit_question.html')
+
+
+
+
+
+
 @app.route("/list")
 def question_list():
     TABLE_HEADERS = ["ID", "DATE", "View", "Vote", "Title", "Message", "Image"]
     all_stats = connection.reader_csv(question_path)
-    return render_template("question_list.html", all_stats=all_stats, TABLE_HEADERS=TABLE_HEADERS)
+    return render_template("index.html", all_stats=all_stats, TABLE_HEADERS=TABLE_HEADERS)
+
+
+
+
+
+
+
+
 
 @app.route("/add-question", methods=["GET", "POST"])
 def add_question():
@@ -57,8 +71,7 @@ def display_question(question_id):
     for answer in answers:
         if answer["question_id"] == str(question_id):
             question_answers.append(answer["message"])
-    return render_template("display_question.html", question_title = question_title, question_message = question_message,
-                           answers=question_answers, question_id=question_id)
+    return render_template("display_question.html", question_title = question_title, question_message = question_message, answers=question_answers, question_id=question_id)
     
     
 @app.route("/question/<question_id>/delete", methods=["POST", "GET"])
@@ -88,6 +101,8 @@ def new_answer(question_id):
                        "message": title_message, "image": ""}
     if request.method == "POST":
         connection.write_data(new_answer_dict, answer_path)
+    if request.method == "POST":
+        return display_question(question_id)
 
     return render_template("new_answer.html", question_title=question_title, question_message=question_message,title_message=title_message, question_id=question_id)
 
